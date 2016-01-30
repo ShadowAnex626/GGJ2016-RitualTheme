9b4b0aa30336327de73e347c12d7e1edc10fbae4
TerryKatsoulis <terry.katsoulis15@gmail.com>
2016-01-29 22:12:33 -0500

    commit 01
    player moves and enemy1 follows

---
+++ GJ2016/Assets/Scripts/Enemy1.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy1 : MonoBehaviour
+{
+    //Follow enemy Variant 1
+
+    // public variable
+    public int health;
+    public int damage;
+    public int power;
+
+
+    //private variables
+    int movementSpeed;
+    GameObject player;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Initializing stat variables
+        health = 20;
+        damage = 5;
+        movementSpeed = 1;
+        power = 1;
+
+        //Finding Player
+        player = GameObject.FindWithTag("Player");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (player)
+        {
+
+            float yDifference = player.transform.position.y - transform.position.y;
+            if (yDifference > 1f)
+            {
+                if (yDifference < 0f)
+                {
+                    transform.Translate(Vector3.down * movementSpeed * Time.deltaTime);
+                }
+                else
+                {
+                    transform.Translate(Vector3.up * movementSpeed * Time.deltaTime);
+                }
+            }
+            else
+            {
+                transform.Translate((player.transform.position - transform.position).normalized * movementSpeed * Time.deltaTime);
+            }
+            float xDifference = player.transform.position.x - transform.position.x;
+            if (xDifference > 1f)
+            {
+                if (xDifference < 0f)
+                {
+                    transform.Translate(Vector3.left * movementSpeed * Time.deltaTime);
+                }
+                else
+                {
+                    transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
+                }
+            }
+            else
+            {
+                transform.Translate((player.transform.position - transform.position).normalized * movementSpeed * Time.deltaTime);
+
+            }
+        }
+
+    }
+}
---
+++ GJ2016/Assets/Scripts/player_move.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player_move : MonoBehaviour {
+
+    public float speed;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+        transform.Translate(new Vector3(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed, 0) * Time.deltaTime);
+
+    }
+}
